7d0ce42dde5b4d30c563c100702eddf59beed7ba
Pasan Chamikara <pasanchamikara989@gmail.com>
2022-10-21 21:17:45 +0530

    login register complete

--- app/src/main/java/com/example/madgroupproject/MainActivity.java
+++ app/src/main/java/com/example/madgroupproject/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.madgroupproject;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,LoginPage.class));
+                finish();
+            }
+        },3000);
     }
 }
