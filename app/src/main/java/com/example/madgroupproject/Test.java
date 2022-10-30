68bd21a254880fcf7b749b261068de9a7981199f
it21099540 <it21099540@my.sliit.lk>
2022-10-30 09:39:35 +0530

    apply test cases

--- app/src/main/java/com/example/madgroupproject/Test.java
+++ app/src/main/java/com/example/madgroupproject/Test.java
@@ -1,2 +1,13 @@
-package com.example.madgroupproject;public class Test {
+package com.example.madgroupproject;
+
+public class Test {
+
+    public String valid(String email){
+        return email;
+
+    }
+
+    public boolean validName(String name){
+        return !(name.isEmpty());
+    }
 }
